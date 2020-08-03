1404b260ddd4ac549ff855822cd758c7c6adcf7f
Ryan Premi <rpremi@hotmail.com>
2020-08-02 22:40:33 -0700

    minor fix

--- R/sing_day.R
+++ R/sing_day.R
@@ -11,19 +11,30 @@
 #' @import dplyr
 #' @import glue
 #' @import purrr
+#' @import english
 #'
 #' @export
+
+library(dplyr)
+library(glue)
+library(purrr)
+library(stringr)
+library(english )
+
 sing_day <- function(dataset, line, phrase_col){
 
   phrases <- dataset %>% pull({{phrase_col}})
   phrases <- phrases[1:line]
-  phrases <- rev(phrases)
+
+  if(substr(phrases[1],1,3) !="and"){
+    phrases <- rev(phrases)
+  }
 
   nms <- c("first", "second", "third", "fourth", "fifth", "sixth" ,"seventh", "eighth", "ninth",
-          "tenth", "eleventh", "twelfth")
+           "tenth", "eleventh", "twelfth")
 
   phrases <- phrases %>%
-                str_replace("^a", "and a")
+    str_replace("^a", "and a")
 
   days <- paste(phrases, collapse='\n')
   res <- glue("On the {nms[line]} day of Christmas, my true love sent to me,","\n{days}")
